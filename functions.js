8f75a1c24551ac7365a1bbef2425e3e4f40f775a
Jaden Brown <jadenmb01@gmail.com>
2021-04-08 17:42:51 -0400

    initial commit

---
+++ functions.js
@@ -0,0 +1,28 @@
+function firstLetterName([name, first]) {
+    alert("The name " + name, "starts with the letter" + first);
+    }
+function divisibleByTwo(x) {
+    if (x%2 == 0)
+    {
+    return true
+    }
+    else
+    {
+    return false
+    }
+}
+function largestNum(arr) {
+    var array = [1, 4, 7, 9, 17];
+var largest= 0;
+
+for (i=0; i<=largest;i++){
+    if (array[i]>largest) {
+        var largest=array[i];
+    }
+}
+    }
+    
+    
+    firstLetterName(Jaden, J);
+    divisibleByTwo(2);
+    largestNum(17);
